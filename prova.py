551acbd145ec4bf9612cd09fb4c781588706ec53
Dionichi <117230003+Dionichi@users.noreply.github.com>
2024-07-13 01:07:12 +0200

    acuatltzació renderitzat i flash

--- prova.py
+++ prova.py
@@ -40,7 +40,7 @@ def readspfile(aFilename):
    f.close()
    print(dir)
 
-def decodecolor(atribut):
+def decodecolor(atribut,special):
    # https://en.wikipedia.org/wiki/ZX_Spectrum_graphic_modes#Colour_palette
    coloret = (
       (0x000000, 0x0100CE, 0xCF0100, 0xCF01CE, 0x00CF15, 0x01CFCF, 0xCFCF15, 0xCFCFCF), # bright 0
@@ -48,13 +48,21 @@ def decodecolor(atribut):
    )
    # http://www.breakintoprogram.co.uk/hardware/computers/zx-spectrum/screen-memory-layout
    bright = (atribut & 0b01000000)>>6
-   return (coloret[bright][atribut & 0b00000111], coloret[bright][(atribut & 0b00111000)>>3])
+   flash = atribut & 0b10000000;
 
-def renderline(lienzo, y, adr_pattern):
+   tinta = coloret[bright][atribut & 0b00000111]
+   paper = coloret[bright][(atribut & 0b00111000)>>3]
+   
+   if (flash >>7 & special ):   
+      return (paper, tinta)
+   else:
+      return (tinta, paper)
+
+def renderline(lienzo, y, adr_pattern, special):
    adr_attributs = 22528 + ((y >> 3)*32)
    x = 0
    for col in range(32):      
-      ink, paper = decodecolor(mem[adr_attributs])
+      ink, paper = decodecolor(mem[adr_attributs], special)
       b = 0b10000000
       while (b>0):
          if ((mem[adr_pattern] & b) == 0):
@@ -66,12 +74,13 @@ def renderline(lienzo, y, adr_pattern):
       adr_pattern = adr_pattern + 1
       adr_attributs = adr_attributs + 1
 
-def renderscreen1(lienzo):
+
+def renderscreen1(lienzo, special):
    dir = 16384
    for y in range(192):
       # 000 tt zzz yyy xxxxx
       offset = ((y & 0b11000000) | ((y & 0b111) << 3) | (y & 0b111000) >> 3) << 5
-      renderline(lienzo, y, dir+offset)
+      renderline(lienzo, y, dir+offset, special)
 
 
 def renderscreen2(lienzo):
@@ -86,21 +95,41 @@ def renderscreen2(lienzo):
 
 print("inici")
 #readz80file("jocs/Jet Set Willy (1984)(Software Projects).z80")
-readsnafile("jocs/uridium.sna")
+#readsnafile("jocs/uridium.sna")
 #readspfile("jocs/GEOGRAP.SP")
 #readspfile("jocs/KNLORE.SP")
+readsnafile("Jetman.sna")
 
 pygame.init()
-screen = pygame.display.set_mode((256*4, 200*4))
+screen = pygame.display.set_mode((256, 192), pygame.SCALED,  vsync=1)
 pygame.display.set_caption("Hello World")
-surface = pygame.Surface((256, 200))
-renderscreen1(surface)
-surfacegran = pygame.transform.scale(surface, (256*4, 200*4))
-screen.blit(surfacegran, (0, 0))
-pygame.display.flip()
+
+clock = pygame.time.Clock()
+clock.tick(50) 
+
+conta = 0
+copia = 0
 
 while True:
+
+  #print ('tick={}, fps={}'.format(clock.tick(60), clock.get_fps()))
+   conta = conta +1
+
+   if (conta & 0b00100000):
+      renderscreen1(screen,0)      
+   else:
+      renderscreen1(screen,1)         
+
+   pygame.display.flip()
+
    for event in pygame.event.get():
+
       if event.type == pygame.QUIT:
          pygame.quit()
          sys.exit()
+    
+      
+
+      
+
+ 
